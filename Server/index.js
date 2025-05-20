9b0b75a0454958c846d9c1dd65f8d84a52d210dc
Ritesh Bamola <rishubamola01@gmail.com>
2025-05-20 12:56:20 +0530

    update index.js - rider routes

--- Server/index.js
+++ Server/index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const { config } = require('dotenv');
 const userRoute = require('./Routes/user');
-const requestRoute = require('./Routes/request')
+const requestRoute = require('./Routes/request');
+const riderRoute = require('./Routes/admin')
 const db = require('./db/db');
 config();
 
@@ -15,6 +16,7 @@ app.use(express.json()); // parse JSON bodies
 
 app.use('/api/user', userRoute);
 app.use('/api/user', requestRoute);
+app.use('/api/rider', riderRoute);
 app.get('/', (req, res) => res.send('Hello World!'));
 
 app.listen(port, () => {
