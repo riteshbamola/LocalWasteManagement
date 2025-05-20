9904d26ff565453b78741ec943da46dd8b9474da
Ritesh Bamola <rishubamola01@gmail.com>
2025-05-20 09:39:19 +0530

    update Routes/request.js
    Added delete and get all request route

--- Server/Routes/request.js
+++ Server/Routes/request.js
@@ -22,4 +22,34 @@ router.post('/postrequest', authenticateToken, (req, res) => {
     res.status(400).json({ message: error.message });
   }
 })
+
+router.get('/getrequest', authenticateToken, async (req, res) => {
+  try {
+    const user = req.user;
+    const userId = user.id;
+
+    const allrequ = await Request.find({ userId: userId }).sort({ createdAt: -1 })
+      .then((allrequ) => {
+        res.json(allrequ);
+      })
+      .catch((err) => {
+        res.status(400).json({ message: err.message });
+      });
+  }
+  catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+})
+router.delete('/deleterequest', async (req, res) => {
+  try {
+    // const user = req.user;
+    const requestid = req.query.requestid;
+    await Request.findByIdAndDelete(requestid).then((requ) => {
+      res.json({ message: "req deleted", requ })
+    })
+  } catch (error) {
+    res.status(400).json({ message: error.message })
+  }
+})
+
 module.exports = router;
