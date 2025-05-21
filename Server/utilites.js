00954525ac144b8003852ab9532f8ba477d46aa3
Ritesh Bamola <rishubamola01@gmail.com>
2025-05-21 22:39:25 +0530

    random commit

--- Server/utilites.js
+++ Server/utilites.js
@@ -16,22 +16,7 @@ function authenticateToken(req, res, next) {
     next();
   })
 }
-// function authenticateTokenRider(req, res, next) {
-//   const authHeader = req.headers['authorization'];
-//   const token = authHeader;
-//   if (!token) return res.status(400).json({ message: "401" });
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN, (err, rider) => {
-//     // console.log(user)
-//     if (err) {
-//       console.log("User not geenrated"); return res.status(401).json({ msg: "Invalid or expired token" });
-//     }
-//     req.rider = rider;
-
-//     next();
-//   })
-
-// }
 module.exports = {
   authenticateToken,
 
